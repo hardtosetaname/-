8c32c7eccf06095766e25802f4e3cc2d14cd4927
耶！ <53459081+hardtosetaname@users.noreply.github.com>
2020-03-19 12:03:20 +0800

    Create wifi_servo.ino

---
+++ wifi_servo.ino
@@ -0,0 +1,33 @@
+#include <ESP8266WiFi.h>
+#include <Servo.h>
+Servo servox;
+Servo servoy;
+
+const char* Ssid     = "@PHICOMM_BC";
+const char* Password = "lijiangchun112";
+
+const char* Server = "192.168.2.175";
+const int Port = 2666;
+
+bool Flag = false;
+WiFiClient client;
+
+void setup() {
+  WiFi.begin(Ssid, Password);
+  servox.attach(2);
+  servoy.attach(4);
+}
+
+void loop() {
+  unsigned char asa[3] = {0};
+  if(Flag==false && WiFi.status()==WL_CONNECTED)
+  {
+    Flag=true;
+    client.connect(Server, Port);
+  }
+  while(client.available()){ 
+    client.read(asa,2);
+    servox.write(asa[0]);
+    servoy.write(asa[1]);
+  }
+}
